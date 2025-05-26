02397628eb197f776b3febe1326c2a1ae609f674
emfo2405 <emfo2405@student.miun.se>
2025-05-26 14:53:16 +0200

    applikation startad

--- server.js
+++ server.js
@@ -3,4 +3,9 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log("Server running at http://localhost:" + port);
+})
